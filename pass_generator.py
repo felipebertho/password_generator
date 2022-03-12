0c68aa26aa9b97d122ec8cd64901b767d1a6e195
Fez1ca <felipebertho@icloud.com>
2022-03-12 17:44:19 -0300

    Implementação de console para continuar gerando senha

--- pass_generator.py
+++ pass_generator.py
@@ -1,30 +1,35 @@
 import random
 
-uppercase_letters = "QWERTYUIOPASDFGHJKLZXCVBNM"
-lowercase_letters = uppercase_letters.lower()
-digits_numbers = "0123456789"
-symbols = "!@#$%¨&*()-+_"
-
-upper, lower, nums, syms = True, True, True, True
-
-all = ""
-
-if upper:
-    all += uppercase_letters
-if lower:
-    all += lowercase_letters
-if nums:
-    all += digits_numbers
-if syms:
-    all += symbols
-
-length = 8
-amount = 1
-
-for x in range(amount):
-    password = "".join(random.sample(all, length))
-    print(password)
+resposta=input("Deseja gerar uma senha? ").upper()
+#Função de usar continuamente.
 
 
+while resposta != "NAO":
+
+
+    uppercase_letters = "QWERTYUIOPASDFGHJKLZXCVBNM"
+    lowercase_letters = uppercase_letters.lower()
+    digits_numbers = "0123456789"
+    symbols = "!@()#"
+    upper, lower, nums, syms = True, True, True, True
+    all = ""
+    if upper:
+        all += uppercase_letters
+    if lower:
+        all += lowercase_letters
+    if nums:
+        all += digits_numbers
+    if syms:
+        all += symbols
+    length = 8
+    amount = 1
+    for x in range(amount):
+        password = "".join(random.sample(all, length))
+        print("Senha gerada: " + password)
+    resposta=input("Deseja gerar outra senha? ")
+    if resposta=="nao":
+
+        print("O programa foi fechado.")
+        break
 
 
